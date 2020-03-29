d2c288528f284a9eeaa22b3b19e038233adac1ad
Benjamin <benjamin.guy.thomas@gmail.com>
2020-03-29 18:07:10 +0200

    Add: 6-kyu/consecutive-strings/java

--- 6-kyu/consecutive-strings/java/src/Words.java
+++ 6-kyu/consecutive-strings/java/src/Words.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Words {
 
     public static String longestConsec(String[] words, int n) {
-        System.out.println(n);
-        return words[0];
+        if (n > words.length) {
+            return "";
+        }
+        final String[] combine = combine(words, n);
+        Arrays.sort(combine, Comparator.comparingInt(String::length).reversed());
+        return combine[0];
     }
 
     public static String[] combine(String[] words, int n) {
@@ -10,13 +17,23 @@ public class Words {
         String[] words2 = new String[len];
 
         for (int i = 0; i < len; i++) {
-            String word = "";
+            StringBuilder word = new StringBuilder();
             for (int j = 0; j < n; j++) {
-                word += words[i + j];
+                word.append(words[i + j]);
             }
-            System.out.println("word =" + word);
-            words2[i] = word;
+            words2[i] = word.toString();
         }
         return words2;
+    }
+
+    public static String[][] slide(String[] list, int inGroupsOf) {
+        int newSize = list.length - inGroupsOf + 1;
+        String[][] res = new String[newSize][inGroupsOf];
+        for (int i = 0; i < newSize; i++) {
+            for (int j = 0; j < inGroupsOf; j++) {
+                res[i][j] = list[i+j];
+            }
+        }
+        return res;
     }
 }
--- 6-kyu/consecutive-strings/java/src/WordsTest.java
+++ 6-kyu/consecutive-strings/java/src/WordsTest.java
@@ -3,8 +3,43 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WordsTest {
+
+    @Test
+    public void slidingWindow() {
+
+        assertArrayEquals(new String[][]{
+                {"a", "b", "c"},
+                {"b", "c", "d"},
+                {"c", "d", "e"},
+                {"d", "e", "f"}
+        }, Words.slide(new String[]{"a", "b", "c", "d", "e", "f"}, 3));
+
+        assertArrayEquals(new String[][]{
+                {"a", "b"},
+                {"b", "c"},
+                {"c", "d"},
+                {"d", "e"},
+                {"e", "f"},
+        }, Words.slide(new String[]{"a", "b", "c", "d", "e", "f"}, 2));
+
+        assertArrayEquals(new String[][]{
+                {"a"},
+                {"b"},
+                {"c"},
+                {"d"},
+                {"e"},
+                {"f"},
+        }, Words.slide(new String[]{"a", "b", "c", "d", "e", "f"}, 1));
+
+        assertArrayEquals(new String[][]{
+                {"a", "b", "c"},
+                {"b", "c", "d"},
+                {"c", "d", "e"},
+        }, Words.slide(new String[]{"a", "b", "c", "d", "e"}, 3));
+    }
 
     @Test
     public void combine() {
@@ -32,5 +67,22 @@ public class WordsTest {
                 Words.combine(List.of("aa", "bb", "cc").toArray(String[]::new), 3)
         );
 
+
+    }
+
+    @Test
+    public void longestConsec() {
+        assertEquals("abigailtheta",
+                Words.longestConsec(new String[]{"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2)
+        );
+        assertEquals("oocccffuucccjjjkkkjyyyeehh",
+                Words.longestConsec(new String[] {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, 1)
+        );
+        assertEquals("",
+                Words.longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas"}, -2)
+        );
+        assertEquals("",
+                Words.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 15)
+        );
     }
 }
